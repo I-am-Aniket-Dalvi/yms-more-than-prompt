e3b4aae12cf690c4699a822124c3f96902c36c41
I-am-Aniket-Dalvi <aniketd741@gmail.com>
2025-10-08 12:20:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     allowedHosts: [
       "aaea0a7b-8a78-4a64-97ac-16d3dd6b26b7-00-20z77mbhdwtz4.worf.replit.dev",
+      "360db694-297b-4c97-a461-40b502b08505-00-37annspeffmvq.spock.replit.dev",
     ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
